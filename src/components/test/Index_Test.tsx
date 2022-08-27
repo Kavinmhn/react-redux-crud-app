fdd7970e199fdab9c20c64b2282feae03176595b
Kavinmhn <kavinmohan.coder@gmail.com>
2022-08-26 18:57:01 -0700

    Jest related files

--- src/components/test/Index_Test.tsx
+++ src/components/test/Index_Test.tsx
@@ -10,8 +10,10 @@ it("Smoke Test: renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <Provider store={configureStore()}>
-      <App />
-    </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Provider>,
     div
   );
 });
@@ -21,8 +23,10 @@ it("Snapshot Test: renders without crashing", () => {
   const tree = renderer
     .create(
       <Provider store={configureStore()}>
-        <App />
-      </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Provider>
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
